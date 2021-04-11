04685ebb1a7af4dc9066d90a284dbede1dbadab2
Salai-Rares <salaicostel@yahoo.com>
2021-04-11 13:58:18 +0300

    created repository package + UserRepository and VerificationTokenRepository interfaces

---
+++ src/main/java/com/example/Reddit_clone/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.Reddit_clone.repository;
+import com.example.Reddit_clone.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
