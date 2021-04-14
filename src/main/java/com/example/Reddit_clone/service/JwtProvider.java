720aadf2f07ba2f1fafe4bbae6e5dbf0f50e3744
razalberto <raz_alberto@yahoo.com>
2021-04-14 14:24:33 +0300

    JwtProvider class and springblog.jks (Java keystore file) are created

---
+++ src/main/java/com/example/Reddit_clone/service/JwtProvider.java
@@ -0,0 +1,47 @@
+package com.example.Reddit_clone.service;
+
+import com.example.Reddit_clone.exceptions.SpringRedditException;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
+// We are using this class to create out JSON Web Token(JWT)
+@Service
+public class JwtProvider {
+
+    private KeyStore keyStore;
+
+    @PostConstruct
+    public void init() {
+        try {
+            keyStore = KeyStore.getInstance("JKS");
+            InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
+            keyStore.load(resourceAsStream, "secret".toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+            throw new SpringRedditException("Exception occurred while loading keystore");
+        }
+
+    }
+    //We are using AsymmetricEncryption to sign our JWT’s using Java Keystore (using Public-Private Key)
+    public String generateToken(Authentication authentication) {
+        org.springframework.security.core.userdetails.User principal = (User) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject(principal.getUsername())
+                .signWith(getPrivateKey())
+                .compact();
+    }
+
+    private PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey) keyStore.getKey("springblog", "secret".toCharArray());// read the private key from our keystore
+        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+            throw new SpringRedditException("Exception occured while retrieving public key from keystore");
+        }
+    }
+}
